a98b11f7dab1a3ed8c478fe3224b5f70e99e7f54
Jakub Wlodek <jakub.wlodek@stonybrook.edu>
2018-04-02 17:59:45 -0400

    Added an src folder and a temporary main file

---
+++ src/RowersMeetMain.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ *  Main classs for Rowers Meet.
+ * 
+ * @author Jakub Wlodek
+ *
+ */
+public class RowersMeetMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
