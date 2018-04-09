f09f24137d975e7879d6c974a7c704e9af3c28af
Jakub Wlodek <jakub.wlodek@stonybrook.edu>
2018-04-09 18:47:40 -0400

    Fixed Validate method

--- src/Main/Validate.java
+++ src/Main/Validate.java
@@ -5,34 +5,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Validate {
-    public static boolean checkUser(String email,String pass) 
-    {
-    	System.out.println(email);
-    	if(email.equals("Correct")) {
-    		return true;
-    	} else {
-    		return false;
-    	}
-     //boolean st =false;
-     //try{
-	 //loading drivers for mysql
-       // Class.forName("com.mysql.jdbc.Driver");
+	public static boolean checkUser(String email,String pass) 
+	{
+		boolean st = false;
+		
+		try{
+			//loading drivers for mysql
+			Class.forName("com.mysql.jdbc.Driver");
 
-	 //creating connection with the database 
-        Connection con=DriverManager.getConnection
-                       ("jdbc:mysql:/ /mysql-305project.cwetgjbvvo5t.us-west-2.rds.amazonaws.com:3306","root","password");
-       PreparedStatement ps =con.prepareStatement
-                            ("select * from Person where Email=? and Password=?");
-        ps.setString(1, email);
-        ps.setString(2, pass);
-        ResultSet rs =ps.executeQuery();
-        st = rs.next();
-       
-     }catch(Exception e)
-     {
-         e.printStackTrace();
-     }
-	 return st;
+					//creating connection with the database 
+					Connection con=DriverManager.getConnection
+							("jdbc:mysql:/ /mysql-305project.cwetgjbvvo5t.us-west-2.rds.amazonaws.com:3306","root","password");
+					PreparedStatement ps =con.prepareStatement
+							("select * from Person where Email=? and Password=?");
+					ps.setString(1, email);
+					ps.setString(2, pass);
+					ResultSet rs =ps.executeQuery();
+					st = rs.next();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return st;
 
- }  
+	}  
 }
