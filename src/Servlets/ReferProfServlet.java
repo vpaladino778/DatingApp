03125aed44a205cf3c2e8e4e432ccd5069de3136
Jakub Wlodek <jakub.wlodek@stonybrook.edu>
2018-05-03 03:39:16 -0400

    added referral support

---
+++ src/Servlets/ReferProfServlet.java
@@ -0,0 +1,37 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Main.EmployeeHome;
+import Main.SQLAccessor;
+import Main.UserDat;
+
+public class ReferProfServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		SQLAccessor sqlA = new SQLAccessor();
+		String prof1 = request.getParameter("prof1");
+		String prof2 = request.getParameter("profile2");
+		try {
+			System.out.println(UserDat.ps1.getProfileID()+", "+prof1+", "+ prof2);
+			sqlA.refferProfile(UserDat.ps1.getProfileID(), prof1, prof2);
+			EmployeeHome.loadEmployeeHome(request, response, sqlA);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	} 
+}
+
